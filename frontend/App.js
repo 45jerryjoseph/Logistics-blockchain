95a614f4a1e78df6199a6081af3cad6c6d9d9b2b
collins-ruto <collinsruto48@gmail.com>
2022-07-24 22:10:16 +0300

    ui updates

--- frontend/App.js
+++ frontend/App.js
@@ -8,7 +8,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { login } from "./assets/js/near/utils";
 import getConfig from "./assets/js/near/config";
 import Supplier from "./pages/supplier/Supplier";
-import Manage from "./pages/supplier/Manage";
 import Donors from "./pages/Donors/Donors";
 import Manage from "./pages/components/Manage";
 import Hospital from "./pages/hospitals/Hospital"
@@ -61,7 +60,7 @@ export default function App() {
       <Routes>
         <Route exact path="/supplier" element={<Supplier />}/>
         <Route exact path="/donors" element={<Donors />}/>
-          <Route exact path="/supplier/manage" element={<Manage />} />
+        <Route exact path="/supplier/manage" element={<Manage />} />
         <Route exact path="/supplier" element={<Supplier />} />
         <Route exact path="/supplier/manage" element={<Manage />} />
         <Route exact path="/hospital" element={<Hospital />} />
